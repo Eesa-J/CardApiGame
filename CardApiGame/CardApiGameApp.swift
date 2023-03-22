09709930c2bba3e71c376e639bedd67dd06ec65a
Eesa Jaswal <jaswal.e@etoncollege.org.uk>
2023-03-22 09:56:55 +0000

    Initial Commit

---
+++ CardApiGame/CardApiGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardApiGameApp.swift
+//  CardApiGame
+//
+//  Created by Jaswal, Eesa (SPH) on 22/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct CardApiGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
